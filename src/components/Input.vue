a08d1482e7726159230416dab6802ac21fb964fb
webfansplz <308241863@qq.com>
2022-12-06 21:00:56 +0800

    fix(input): use `event.data` instead of `event.key` for chinese characters

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -155,7 +155,7 @@ onInputData((evt) => {
           ) {
             text.value
               = text.value.slice(0, cursorPosition.value)
-              + event.key
+              + evt.data
               + text.value.slice(cursorPosition.value)
             cursorPosition.value++
           }
